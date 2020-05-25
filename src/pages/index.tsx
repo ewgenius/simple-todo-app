fd01458ed296e42d320679db76e482c588cd79d2
Evgenii Khramkov <ewgeniux@gmail.com>
2020-05-25 15:33:14 -0700

    setup tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto max-w-screen-sm">
+      <Head>
+        <title>Simple Todo App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      test
+    </div>
+  );
+}
